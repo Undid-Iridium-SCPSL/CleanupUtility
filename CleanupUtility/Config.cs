e2a4c38ca2e4ff428de0c7e1bd773a9c27aa9f52
undid-iridium <francis.x.irizarry@gmail.com>
2022-04-17 12:47:51 -0400

    Add project files.

---
+++ CleanupUtility/Config.cs
@@ -0,0 +1,25 @@
+﻿using Exiled.API.Interfaces;
+using System.Collections.Generic;
+using System.ComponentModel;
+using static SharedLogicOrchestrator.DebugFilters;
+
+namespace CleanupUtility
+{
+	public sealed class Config : IConfig
+	{
+		[Description("Whether to enable or disable plugin")]
+		public bool IsEnabled { get; set; } = true;
+
+		[Description("The message to show most debug messages.")]
+		public bool debugEnabled { get; set; } = false;
+
+		public Dictionary<DebugFilter, bool> DebugFilters { get; set; } =
+		   new Dictionary<DebugFilter, bool> {
+
+			   {  DebugFilter.All , false },
+			   {  DebugFilter.Fine , false },
+			   {  DebugFilter.Finer , false },
+			   {  DebugFilter.Finest , false }
+		   };
+	}
+}
