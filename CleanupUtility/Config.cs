988455930e5ce3f63db44a910cf922f7ef99bbd0
Build <jwbradley2003@gmail.com>
2022-04-18 05:55:42 -0400

    Update default item filter

--- CleanupUtility/Config.cs
+++ CleanupUtility/Config.cs
@@ -30,12 +30,55 @@ namespace CleanupUtility
         public float CheckInterval { get; set; } = 2f;
 
         /// <summary>
-        /// Gets or sets the item filter.
+        /// Gets or sets a collection of items that should be deleted paired with the time, in seconds, to wait before deleting them.
         /// </summary>
-        [Description("The item filter. If you want an item to be removed, add it here with the time in seconds.")]
+        [Description("A collection of items that should be deleted paired with the time, in seconds, to wait before deleting them.")]
         public Dictionary<ItemType, float> ItemFilter { get; set; } = new()
         {
-            { ItemType.GrenadeHE, 15f },
+            { ItemType.KeycardJanitor, 600f },
+            { ItemType.KeycardScientist, 600f },
+            { ItemType.KeycardResearchCoordinator, 600f },
+            { ItemType.KeycardZoneManager, 600f },
+            { ItemType.KeycardGuard, 600f },
+            { ItemType.KeycardNTFOfficer, 600f },
+            { ItemType.KeycardContainmentEngineer, 600f },
+            { ItemType.KeycardNTFLieutenant, 600f },
+            { ItemType.KeycardNTFCommander, 600f },
+            { ItemType.KeycardFacilityManager, 600f },
+            { ItemType.KeycardChaosInsurgency, 600f },
+            { ItemType.KeycardO5, 600f },
+            { ItemType.Radio, 600f },
+            { ItemType.GunCOM15, 600f },
+            { ItemType.Medkit, 600f },
+            { ItemType.Flashlight, 600f },
+            { ItemType.SCP500, 600f },
+            { ItemType.SCP207, 600f },
+            { ItemType.Ammo12gauge, 600f },
+            { ItemType.GunE11SR, 600f },
+            { ItemType.GunCrossvec, 600f },
+            { ItemType.Ammo556x45, 600f },
+            { ItemType.GunFSP9, 600f },
+            { ItemType.GunLogicer, 600f },
+            { ItemType.GrenadeHE, 600f },
+            { ItemType.GrenadeFlash, 600f },
+            { ItemType.Ammo44cal, 600f },
+            { ItemType.Ammo762x39, 600f },
+            { ItemType.Ammo9x19, 600f },
+            { ItemType.GunCOM18, 600f },
+            { ItemType.SCP018, 600f },
+            { ItemType.SCP268, 600f },
+            { ItemType.Adrenaline, 600f },
+            { ItemType.Painkillers, 600f },
+            { ItemType.Coin, 600f },
+            { ItemType.ArmorLight, 600f },
+            { ItemType.ArmorCombat, 600f },
+            { ItemType.ArmorHeavy, 600f },
+            { ItemType.GunRevolver, 600f },
+            { ItemType.GunAK, 600f },
+            { ItemType.GunShotgun, 600f },
+            { ItemType.SCP330, 600f },
+            { ItemType.SCP2176, 600f },
+            { ItemType.SCP1853, 600f },
         };
     }
 }
