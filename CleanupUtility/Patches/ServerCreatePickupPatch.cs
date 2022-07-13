f8ea52d846551ab796e43cb44fe6b99a184cb354
undid-iridium <francis.x.irizarry@gmail.com>
2022-07-13 19:19:29 -0400

    Update cleanup utility for new exiled 5.3

--- CleanupUtility/Patches/ServerCreatePickupPatch.cs
+++ CleanupUtility/Patches/ServerCreatePickupPatch.cs
@@ -17,6 +17,8 @@ namespace CleanupUtility.Patches
     using Exiled.API.Features.Items;
     using HarmonyLib;
     using InventorySystem;
+    using InventorySystem.Items;
+    using InventorySystem.Items.Pickups;
     using NorthwoodLib.Pools;
     using static HarmonyLib.AccessTools;
 
@@ -41,44 +43,44 @@ namespace CleanupUtility.Patches
             LocalBuilder exceptionObject = generator.DeclareLocal(typeof(Exception));
 
             // Our Catch (Try wrapper) block
-            ExceptionBlock catchBlock = new (ExceptionBlockType.BeginCatchBlock, typeof(Exception));
+            ExceptionBlock catchBlock = new(ExceptionBlockType.BeginCatchBlock, typeof(Exception));
 
             // Our Exception handling start
-            ExceptionBlock exceptionStart = new (ExceptionBlockType.BeginExceptionBlock, typeof(Exception));
+            ExceptionBlock exceptionStart = new(ExceptionBlockType.BeginExceptionBlock, typeof(Exception));
 
             // Our Exception handling end
-            ExceptionBlock exceptionEnd = new (ExceptionBlockType.EndExceptionBlock);
+            ExceptionBlock exceptionEnd = new(ExceptionBlockType.EndExceptionBlock);
 
-            List<CodeInstruction> instructionToAdd = new ()
+            List<CodeInstruction> instructionToAdd = new()
             {
                 // Load ItemBase to EStack
                 new CodeInstruction(OpCodes.Ldarg_0).WithBlocks(exceptionStart).MoveLabelsFrom(newInstructions[index]),
 
                 // Assign unspecified enum as the default value, and then try to load the actual value
-                new (OpCodes.Ldc_I4_4),
+                new(OpCodes.Ldc_I4_4),
 
                 // Then save the player zone to a local variable (This is all done early because spawn deletes information and made it default to surface)
-                new (OpCodes.Stloc, itemZone.LocalIndex),
+                new(OpCodes.Stloc, itemZone.LocalIndex),
 
                 // Load EStack to callvirt and get owner back on Estack
-                new (OpCodes.Callvirt, PropertyGetter(typeof(Inventory), nameof(Inventory.gameObject))),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(Inventory), nameof(Inventory.gameObject))),
 
-                new (OpCodes.Call, Method(typeof(ReferenceHub), nameof(ReferenceHub.GetHub), new[] { typeof(UnityEngine.GameObject) })),
+                new(OpCodes.Call, Method(typeof(ReferenceHub), nameof(ReferenceHub.GetHub), new[] { typeof(UnityEngine.GameObject) })),
 
                 // Duplicate ItemBase.Owner (if null then two nulls)
-                new (OpCodes.Dup),
+                new(OpCodes.Dup),
 
                 // If previous owner is null, escape this, still have one null on stack if that is the case
-                new (OpCodes.Brfalse_S, skipLabel),
+                new(OpCodes.Brfalse_S, skipLabel),
 
                 // Using Owner call Player.Get static method with it (Reference hub) and get a Player back, OK game object could be null
-                new (OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
+                new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
 
                 // Then get the player Zone (This was probably null)
-                new (OpCodes.Callvirt, PropertyGetter(typeof(Player), nameof(Player.Zone))),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(Player), nameof(Player.Zone))),
 
                 // Continue without calling broken escape route
-                new (OpCodes.Br_S, continueProcessing),
+                new(OpCodes.Br_S, continueProcessing),
 
                 // Remove current null from stack. Default value setting ZoneType to unspecified if previous owner is null by escaping to this label
                 new CodeInstruction(OpCodes.Pop).WithLabels(skipLabel),
@@ -87,7 +89,7 @@ namespace CleanupUtility.Patches
                 new CodeInstruction(OpCodes.Stloc, itemZone.LocalIndex).WithLabels(continueProcessing),
 
                 // Correctly esacpes try-catch block.
-                new (OpCodes.Leave_S, skipException),
+                new(OpCodes.Leave_S, skipException),
 
                 // Load the exception from stack
                 new CodeInstruction(OpCodes.Nop).WithBlocks(catchBlock).WithBlocks(exceptionEnd),
@@ -120,7 +122,7 @@ namespace CleanupUtility.Patches
                 new CodeInstruction(OpCodes.Ldloc_0),
 
                 // Calls pickup method using local variable 0 (Which is on Eval Stack [ItemPickupBase]) and it gets back a Pickup object onto the EStack [Pickup]
-                new CodeInstruction(OpCodes.Call, Method(typeof(Pickup), nameof(Pickup.Get))),
+                new CodeInstruction(OpCodes.Call, Method(typeof(Pickup), nameof(Pickup.Get), new[] {typeof(ItemPickupBase)})),
 
                 // Calls arguemnt 1 from function call unto EStack (Zone)
                 new CodeInstruction(OpCodes.Ldloc, itemZone.LocalIndex),
