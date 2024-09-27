9c8055236a4b6ad242017025fa42e2ae7b86f697
Undid-Iridium <24619207+Undid-Iridium@users.noreply.github.com>
2024-09-27 13:51:56 -0400

    Fixes new generic function issue

--- CleanupUtility/Plugin.cs
+++ CleanupUtility/Plugin.cs
@@ -35,7 +35,7 @@ namespace CleanupUtility
         public override Version RequiredExiledVersion { get; } = new(8, 0, 0);
 
         /// <inheritdoc />
-        public override Version Version { get; } = new(2, 0, 5);
+        public override Version Version { get; } = new(2, 0, 6);
 
         /// <summary>
         /// Gets an instance of the <see cref="PickupChecker"/> class.
